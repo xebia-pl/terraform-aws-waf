32bba4111f494b9fba338cd77f3b7807c37c61bb
Ben de Haan <53901866+bendehaan@users.noreply.github.com>
2021-08-21 10:59:52 +0200

    Add outputs

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,19 @@
-# bucket id
-# bucket arn
-# web acl arn
-# web acl capacity
+output "waf_log_bucket_name" {
+  description = "The name of the WAF log bucket"
+  value       = module.waf_logging_bucket.bucket_id
+}
+
+output "waf_log_bucket_arn" {
+  description = "The arn of the WAF log bucket"
+  value       = module.waf_logging_bucket.bucket_arn
+}
+
+output "waf_web_acl_arn" {
+  description = "The ARN of the WAF Web ACL"
+  value       = aws_wafv2_web_acl.web_acl.arn
+}
+
+output "waf_web_acl_capacity" {
+  description = "The capacity of the WAF Web ACL"
+  value       = aws_wafv2_web_acl.web_acl.capacity
+}
